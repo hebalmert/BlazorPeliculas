648ab4b8589fff026bc6cbc33dc32687ed685543
Hebert Merchan <merchanhebert@gmail.com>
2024-10-01 22:43:46 -0400

    The last part was how implement LazyLoader

--- BlazorPeliculas/BlazorPeliculas.Client/Pages/Counter.razor.cs
+++ BlazorPeliculas/BlazorPeliculas.Client/Pages/Counter.razor.cs
@@ -1,4 +1,4 @@
-﻿using BlazorPeliculas.Client.Helpers;
+﻿using MathNet.Numerics.Statistics;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
@@ -7,9 +7,16 @@ namespace BlazorPeliculas.Client.Pages
     public partial class Counter
     {
         private int currentCount = 0;
+        [Inject] public IJSRuntime js { get; set; } = null!;
 
-        public void IncrementCount()
+        public async Task IncrementCount()
         {
+            var arreglo = new double[] { 1, 2, 3, 4, 5 };
+            var Max = arreglo.Maximum();
+            var Min = arreglo.Minimum();
+
+            await js.InvokeVoidAsync("alert", $"El Max es {Max} y el min es {Min}");
+
             currentCount++;
         }
     }
